404ed24f9c1d1fc48baecf6c97cc2f1f3cab7187
JavoGnrea79 <133068477+MRGonorrea79@users.noreply.github.com>
2025-10-02 23:11:10 -0500

    Add files via upload

---
+++ GR03_1BT2_622_25B/src/main/java/Entidades/Usuario.java
@@ -0,0 +1,46 @@
+package Entidades;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario") 
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Basic
+    private String username;
+    private String password;
+
+    public Usuario() {}
+
+    public Usuario(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+ 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ GR03_1BT2_622_25B/src/main/java/Persistencia/PersistenceController.java
@@ -0,0 +1,14 @@
+
+package Persistencia;
+
+import Entidades.Usuario;
+import java.util.List;
+
+
+public class PersistenceController {
+    UsuarioJpaController usuJpa = new UsuarioJpaController();
+    
+    public List<Usuario> getUser(){
+        return usuJpa.findUsuarioEntities();
+    }
+}
